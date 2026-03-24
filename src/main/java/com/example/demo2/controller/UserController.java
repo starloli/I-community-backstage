73bcb3451f9bb492e61db4defc2e37d0a21d39c5
starlo <saoend27@gmail.com>
2026-03-24 13:49:38 +0800

    0

--- src/main/java/com/example/demo2/controller/UserController.java
+++ src/main/java/com/example/demo2/controller/UserController.java
@@ -1,13 +1,21 @@
 package com.example.demo2.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo2.dto.request.FacilityRegistRequest;
+import com.example.demo2.dto.response.FacilityResponse;
 import com.example.demo2.dto.response.UserResponse;
+import com.example.demo2.service.FacilityService;
 import com.example.demo2.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -19,12 +27,24 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
     private final UserService userService;
+    private final FacilityService facilityService;
+
 
     @GetMapping("/me")
     public ResponseEntity<UserResponse> getMe(
-        Authentication authentication
-    ) {
+            Authentication authentication) {
         String name = authentication.getName();
         return ResponseEntity.ok(userService.getProfileByName(name));
+    }
+
+    @PostMapping("/regist-facility")
+    public ResponseEntity<FacilityResponse> registFacility(
+            @RequestBody FacilityRegistRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(facilityService.registFacility(request));
+    }
+
+    @GetMapping("/facilities")
+    public ResponseEntity<ArrayList<FacilityResponse>> getFacilities() {
+        return ResponseEntity.ok(facilityService.getFacilities());
     }
 }
