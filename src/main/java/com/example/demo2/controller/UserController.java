8901b925b1561327ff941f38677d649fa0482fa6
starlo <saoend27@gmail.com>
2026-03-24 14:02:18 +0800

    0

--- src/main/java/com/example/demo2/controller/UserController.java
+++ src/main/java/com/example/demo2/controller/UserController.java
@@ -4,19 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo2.dto.request.FacilityRegistRequest;
-import com.example.demo2.dto.response.FacilityResponse;
-=======
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
->>>>>>> parent of 5ce4729 (1)
 import com.example.demo2.dto.response.UserResponse;
 import com.example.demo2.service.UserService;
 
@@ -37,18 +27,4 @@ public class UserController {
         String name = authentication.getName();
         return ResponseEntity.ok(userService.getProfileByName(name));
     }
-<<<<<<< HEAD
-
-    @PostMapping("/regist-facility")
-    public ResponseEntity<FacilityResponse> registFacility(
-            @RequestBody FacilityRegistRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(facilityService.registFacility(request));
-    }
-
-    @GetMapping("/facilities")
-    public ResponseEntity<ArrayList<FacilityResponse>> getFacilities() {
-        return ResponseEntity.ok(facilityService.getFacilities());
-    }
-=======
->>>>>>> parent of 5ce4729 (1)
 }
